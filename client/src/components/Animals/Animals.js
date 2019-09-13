98e9e2708963bbe9099417d10fc5295c3b0ecca6
JC Cheney <incontextseo@gmail.com>
2019-09-12 21:24:26 -0500

    Stash

--- client/src/components/Animals/Animals.js
+++ client/src/components/Animals/Animals.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react'
 import './Animals.css'
 
-function Animals(props) {
+// const props = { animalID, animalName, animalGeneralAge, animalThumbnailUrl, animalGeneralSizePotential, animalBreed, animalDescriptionPlain, onClick, buttonText};
 
-    const { animalID, animalName, animalGeneralAge, animalThumbnailUrl, animalGeneralSizePotential, animalBreed, animalDescriptionPlain, onClick, buttonText} = props;
+class Animals extends Component {
+    componentWillUnmount() {
+        window.clearTimeout(this.timeout)
+    }
+    render() {
+        return (
+            <div>
+                This is the Search Filter!
+            </div>
+        )
+    }
+}
 
-    return (
+// function Animals(props) {
 
-        <article id={animalID}>
-            <img src={animalThumbnailUrl} alt={animalName} />
-            <h4>{animalName}</h4>
-            <p>Age: {animalGeneralAge || "Age unknown"}</p>
-            <p>Size: {animalGeneralSizePotential || "Size not provided"}</p>
-            <p>Breed: {animalBreed || "Breed unknown"}</p>
-            <p>{animalDescriptionPlain}</p>
-            <button 
-                className="btn waves-effect waves-light" 
-                onClick={onClick}
-                data-id={animalID}
-            >{buttonText}</button>
+//     const { animalID, animalName, animalGeneralAge, animalThumbnailUrl, animalGeneralSizePotential, animalBreed, animalDescriptionPlain, onClick, buttonText} = props;
 
-        </article>
-    )}
+//     return (
+
+//         <article id={animalID}>
+//             <img src={animalThumbnailUrl} alt={animalName} />
+//             <h4>{animalName}</h4>
+//             <p>Age: {animalGeneralAge || "Age unknown"}</p>
+//             <p>Size: {animalGeneralSizePotential || "Size not provided"}</p>
+//             <p>Breed: {animalBreed || "Breed unknown"}</p>
+//             <p>{animalDescriptionPlain}</p>
+//             <button 
+//                 className="btn waves-effect waves-light" 
+//                 onClick={onClick}
+//                 data-id={animalID}
+//             >{buttonText}</button>
+
+//         </article>
+//     )
+// }
 
 export default Animals
