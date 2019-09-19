b37929ae04e6c5cfb618bb5104d70c2ff384d65d
jdclaas <jd_claas@icloud.com>
2019-09-19 16:45:12 -0500

    commit to pull

--- src/App.js
+++ src/App.js
@@ -9,24 +9,30 @@ import History from './pages/History';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Wrapper from './components/Wrapper';
+import Signup from './components/Signup/signup';
+import Login from './components/Login/Login';
+// import Wrapper from './components/Wrapper';
 
+
+ 
 function App() {
   return (
     <Router>
       <div>
         <Header />
         <Nav />
-          <Wrapper>
+          {/* <Wrapper> */}
             <Route exact path="/" component={Home} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/fosters" component={Fosters} />
             <Route exact path="/history" component={History} />
-          </Wrapper>
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+          {/* </Wrapper> */}
         <Footer />
       </div>
     </Router>
   );
-}
+} 
 
 export default App;
